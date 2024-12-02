cb60cec617738af9330fedfb97583b1369199697
Tristan Valcke <valcke49@gmail.com>
2024-12-02 19:57:32 +0100

    format en PEP8

--- main.py
+++ main.py
@@ -1,60 +1,70 @@
 def find_smallest_int(arr):
     return min(arr)
 
+
 def greet(name):
-    #Good Luck (like you need it)
+    # Good Luck (like you need it)
     return "Hello, "+name+" how are you doing today?"
 
-def find_needle(haystack):  #haystack is a list here
+
+def find_needle(haystack):   # haystack is a list here
     position = 0
     for i in haystack:
         if i == 'needle':
-            return('found the needle at position '+str(position))
-        else :
-            position +=1
+            return ('found the needle at position '+str(position))
+        else:
+            position += 1
+
 
 def hero(bullets, dragons):
-    return bullets/2>=dragons
+    return bullets/2 >= dragons
+
 
 def disemvowel(string_):
     stringReturned = ''
     for i in string_:
-        if i.lower() == 'a' or i.lower()=='e' or i.lower()=='i' or i.lower()=='o' or i.lower()=='u':
+        if i.lower() == 'a' or i.lower() == 'e' or i.lower() == 'i' or \
+           i.lower() == 'o' or i.lower() == 'u':
+
             continue
         else:
-            stringReturned+=i
+            stringReturned += i
     return stringReturned
+
 
 def better_than_average(class_points, your_points):
     classAverage = 0
 
     for i in class_points:
         classAverage += i
-    classAverage=classAverage/len(class_points)
+    classAverage = classAverage/len(class_points)
 
     return your_points > classAverage
 
+
 def nb_year(p0, percent, aug, p):
     """
-        p0, 
-        percent, 
-        aug (inhabitants coming or leaving each year), 
+        p0,
+        percent,
+        aug (inhabitants coming or leaving each year),
         p (population to equal or surpass)
     """
     counter = 0
 
     while p0 <= p:
         p0 = int(p0 + p0/100*percent + aug)
-        counter+=1
+        counter += 1
 
     return counter
 
+
 def repeat_str(repeat, string):
-    str2 =''
+    str2 = ''
     while repeat > 0:
-        str2+=string
-        repeat -=1
+        str2 += string
+        repeat -= 1
     return str2
+
 
 def DNA_strand(dna):
     myStr = ''
@@ -69,23 +79,30 @@ def DNA_strand(dna):
             myStr += 'C'
     return myStr
 
+
 def number_to_string(num):
     return str(num)
 
+
 def bmi(weight, height):
     bmi = weight/(height**2)
-    if bmi<=18.5:
+    if bmi <= 18.5:
         return "Underweight"
-    elif bmi<=25:
+    elif bmi <= 25:
         return "Normal"
-    elif bmi <=30:
+    elif bmi <= 30:
         return "Overweight"
-    elif bmi > 30: 
+    elif bmi > 30:
         return "Obese"
-    
-def bmi2(weight, height):       #autre facon de faire celui du dessus
+
+
+def bmi2(weight, height):  # autre facon de faire celui du dessus
     b = weight / height ** 2
-    return ['Underweight', 'Normal', 'Overweight', 'Obese'][(b > 30) + (b > 25) + (b > 18.5)]
+    return (
+        ['Underweight', 'Normal', 'Overweight', 'Obese']
+        [(b > 30) + (b > 25) + (b > 18.5)]
+    )
+
 
 def invert(lst):
     myList = []
@@ -94,12 +111,15 @@ def invert(lst):
         myList.append(ajout)
     return myList
 
+
 def string_to_number(s):
     return int(s)
 
+
 def is_isogram(string):
     string = string.lower()
-    string2=set(string) #set fait un ensemble, donc si on a des doublons ils seront supprimés
+    # set fait un ensemble, donc si on a des doublons ils seront supprimés
+    string2 = set(string)
     return len(string) == len(string2)
 
 
@@ -133,34 +153,47 @@ def is_isogram(string):
 def rps(p1, p2):
     if p1 == p2:
         return "Draw!"
-    elif (p1 == 'rock' and p2 == 'scissors') or (p1 == 'scissors' and p2 == 'paper') or (p1 == 'paper' and p2 == 'rock'):
+    elif (p1 == 'rock' and p2 == 'scissors') or \
+         (p1 == 'scissors' and p2 == 'paper') or \
+         (p1 == 'paper' and p2 == 'rock'):
         return "Player 1 won!"
     else:
         return "Player 2 won!"
-    
+
+
 def no_space(x):
-    lst= x.split(' ')
+    lst = x.split(' ')
     string = ''
     for i in lst:
-        string +=i
+        string += i
     return string
 
 
-##########################################
+def reverse(string):
+    list = []
+    str = ''
+    for i in string:
+        list.append(i)
+    for j in list[::-1]:
+        str += j
+    return str
 
 
-#print(greet('Tris'))
-#print(find_smallest_int([1,2,3,4,5]))
-#print(find_needle(['needle','haystack','needle']))
-#print(hero(10,5))
-#print(disemvowel('Tu es très moche'))
-#print(better_than_average([2, 3], 5))
-#print(nb_year(1000,5,50,1400))
-#print(DNA_strand('TGAC'))
-#print(number_to_string(5))
-#print(bmi(85,1.76))
-#print(invert([1,-2,3]))
-#print(is_isogram('azerty'))
-#print(rps('rock','rock'))
-print(no_space('caca sur ton  doigt'))
+# #########################################
 
+
+# print(greet('Tris'))
+# print(find_smallest_int([1,2,3,4,5]))
+# print(find_needle(['needle','haystack','needle']))
+# print(hero(10,5))
+# print(disemvowel('Tu es très moche'))
+# print(better_than_average([2, 3], 5))
+# print(nb_year(1000,5,50,1400))
+# print(DNA_strand('TGAC'))
+# print(number_to_string(5))
+# print(bmi(85,1.76))
+# print(invert([1,-2,3]))
+# print(is_isogram('azerty'))
+# print(rps('rock','rock'))
+# print(no_space('caca sur ton  doigt'))
+print(reverse('tristan'))
